92d5562c882f24dbcf93682cde9db679c1be1d67
Joao123433 <joaosantos123433@gmail.com>
2024-01-16 18:13:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Calculadora Científica
+Este é um aplicativo de calculadora simples feito em HTML, CSS e TypeScript. Além das operações aritméticas básicas, a calculadora científica oferece funções trigonométricas, logarítmicas, exponenciais e outras funcionalidades avançadas. A interface inclui suporte para entrada de botões e teclado, proporcionando uma experiência completa para cálculos científicos.
+
+## Link para o projeto
+- https://joao123433.github.io/Calculadora-Cientifica
+
+## Recursos
+Seção 1 - Operações Básicas:
+- `Operações Aritméticas:` Adição, Subtração, Multiplicação, Divisão.
+- `Teclas Numéricas:` 0 a 9 e Ponto Decimal.
+- `Funções Especiais:` Limpar (C), Limpar Entrada (CE), Igual (=).
+
+Seção 2 - Funções Científicas:
+- `Funções Trigonométricas:` Seno (Sin), Cosseno (Cos), Tangente (Tan).
+- `Funções Inversas:` Arcosseno (Sin<sup>-1</sup>), Arcocosseno (Cos<sup>-1</sup>), Arcotangente (Tan<sup>-1</sup>).
+- `Outras Funções:` Fatorial (!), Logaritmo Natural (Ln), Porcentagem (%).
+- `Constantes Matemáticas:` π (Pi) e e (Número de Euler).
+
+Seção 3 - Potenciação e Raízes:
+- `Potenciação:` Elevado a uma potência (^), Quadrado (x<sup>2</sup>), Cubo (x<sup>3</sup>).
+- `Raízes:` Raiz Quadrada (<sup>2</sup>√), Raiz Cúbica (<sup>3</sup>√).
---
+++ public/dist/main.js
@@ -0,0 +1,96 @@
+const inputConta = document.querySelector("#inputConta");
+const inputValue = document.querySelector("#inputValue");
+const normalButtons = document.querySelectorAll("button");
+const eventValues = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "];
+let expression = [];
+let screenValue = [];
+function clickKeys(ev) {
+    ev.preventDefault();
+    const value = ev.target.dataset.value;
+    const valueExp = ev.target.dataset.exp;
+    if (value === "C") {
+        clearAll();
+    }
+    else if (value === "CE") {
+        deleteOne();
+    }
+    else if (value === "=") {
+        calculate();
+    }
+    else if (value === "!") {
+        settingVAlues(value, valueExp);
+        factorial();
+    }
+    else {
+        settingVAlues(value, valueExp);
+    }
+}
+function eventKeys(ev) {
+    ev.preventDefault();
+    if (eventValues.includes(ev.key)) {
+        settingVAlues(ev.key);
+    }
+    if (ev.key === "Backspace") {
+        deleteOne();
+    }
+    if (ev.key === "Enter" || ev.key === "=") {
+        calculate();
+    }
+    if (ev.key === "C") {
+        clearAll();
+    }
+}
+function settingVAlues(value, valueExp) {
+    expression.push(valueExp ?? value);
+    screenValue.push(value);
+    inputValue.value = screenValue.join("");
+}
+function clearAll() {
+    inputConta.value = "";
+    inputValue.value = "";
+    screenValue = [];
+    expression = [];
+    inputValue.focus();
+}
+function deleteOne() {
+    if (inputValue.value !== "") {
+        screenValue.pop();
+        expression.pop();
+        inputValue.value = screenValue.join("");
+    }
+    inputValue.focus();
+}
+function calculate() {
+    inputConta.value = "ERROR";
+    let result = eval(expression.join(""));
+    verifyErrors(result);
+}
+function verifyErrors(result) {
+    if (!Number.isNaN(result) && !(result === undefined) && !(Number(result) === Infinity)) {
+        inputConta.value = inputValue.value;
+        inputValue.value = result;
+        endSetup(result);
+    }
+}
+function calcFactorial(a) {
+    if (a === 0) {
+        return 1;
+    }
+    return (a * calcFactorial(a - 1));
+}
+function factorial() {
+    inputConta.value = "ERROR";
+    const result = eval(calcFactorial(eval(expression.join(""))));
+    verifyErrors(result);
+}
+function endSetup(result) {
+    screenValue = [];
+    expression = [];
+    screenValue.push(result);
+    expression.push(result);
+    expression = Array.from(String(expression[0]));
+    screenValue = Array.from(String(screenValue[0]));
+}
+normalButtons.forEach(element => element.addEventListener("click", clickKeys));
+inputValue.addEventListener("keydown", eventKeys);
+document.addEventListener("DOMContentLoaded", () => inputValue.focus());
